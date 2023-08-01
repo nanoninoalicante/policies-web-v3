4769ef86962d28d5dad9ba2ed63ad6fc592915a2
Christopher Hill <cjameshill@gmail.com>
2023-08-01 13:16:52 +0200

    Initial setup nuxt, tailwind, contentful

---
+++ nuxt.config.ts
@@ -0,0 +1,49 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+const generateRoutes = async () => {
+    let url =
+        "https://cdn.contentful.com/spaces/app2ux3l8qni/environments/master/entries";
+
+    let options = {
+        method: "GET",
+        headers: {
+            Authorization: "Bearer O9rDrPc3MXh15k88xsxLhpcjN_Z_faLf14oF2FKzQV8",
+        },
+    };
+
+    const response = await fetch(url, options)
+        .then((res) => res.json())
+        .catch((err) => {
+            console.error("error:" + err);
+            throw err;
+        });
+
+    return response.items.map((item: any) => {
+        if (!item || !item?.fields?.slug) return null;
+        return { slug: item?.fields?.slug, id: item?.sys?.id };
+    });
+};
+export default defineNuxtConfig({
+    devtools: { enabled: true },
+    css: ["~/assets/css/main.css"],
+    ssr: true,
+    postcss: {
+        plugins: {
+            tailwindcss: {},
+            autoprefixer: {},
+        },
+    },
+    nitro: {
+        prerender: {
+            routes: [],
+        },
+    },
+    hooks: {
+        async "nitro:config"(config: any) {
+            const entries = await generateRoutes();
+            for (const entry of entries) {
+                if (!entry || !entry.slug) continue;
+                config.prerender.routes.push("/t/" + entry.slug);
+            }
+        },
+    },
+});
---
+++ pages/404.vue
@@ -0,0 +1,3 @@
+<template>
+    <main class="p-12"><h1 class="font-medium text-lg">404</h1></main>
+</template>
